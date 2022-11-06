e20ec65d12e5b6c37c77afd2659cb943b3fb29da
Samba Diallo <samba.diallo@id-mac007.idtgv.cnit>
2022-11-06 18:22:13 +0100

    client package installed

--- client/src/app/store.js
+++ client/src/app/store.js
@@ -1,8 +1,8 @@
 import { configureStore } from '@reduxjs/toolkit';
-import counterReducer from '../features/counter/counterSlice';
+import tutorialReducer from '../features/tutorial/tutorialSlice';
 
 export const store = configureStore({
   reducer: {
-    counter: counterReducer,
+    tutorials: tutorialReducer
   },
 });
